824049417d8dd9b97412b043cdbcd2c362c5a90e
Rajni <rajnikant12345@gmail.com>
2018-08-29 09:25:25 +0530

    Update hello.go

--- chapter1/hello.go
+++ chapter1/hello.go
@@ -1,9 +1,16 @@
+//this is a comment
+//this is first line of every go parogam.
+// main means this is the main package and our main function is present in this package
 package main
 
+// one go program can import other go packages
+// here we are importing a dofferent package named "fmt"
 import (
 	"fmt"
 )
 
+// this is main function
 func main() {
+	// and this is where we call a function present in "fmt" package
 	fmt.Println("Hello, playground")
 }
