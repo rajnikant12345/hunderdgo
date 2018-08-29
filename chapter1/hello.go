ba043873269b925a5a554b2d37e5eb0d60ee7201
Rajni <rajnikant12345@gmail.com>
2018-08-29 09:30:02 +0530

    Update hello.go

--- chapter1/hello.go
+++ chapter1/hello.go
@@ -4,7 +4,7 @@
 package main
 
 // one go program can import other go packages
-// here we are importing a dofferent package named "fmt"
+// here we are importing a different package named "fmt"
 import (
 	"fmt"
 )
@@ -12,5 +12,14 @@ import (
 // this is main function
 func main() {
 	// and this is where we call a function present in "fmt" package
-	fmt.Println("Hello, playground")
+	fmt.Println("Hello, Gophers....")
 }
+
+
+//Key points:
+/*
+1. A Go programs always begins with packahe keyword.
+2. main() is the entry point for a program , in Go . Just like C or C++ ot Java.
+3. for running a go program
+	a. Use "go run hello.go"
+	b. -- There is another way, but we will discuss that in later part----
